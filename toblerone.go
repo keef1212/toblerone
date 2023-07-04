0a6204d3790217456e3c0434c1114f524f1db0c1
keef <134746972+keef1212@users.noreply.github.com>
2023-07-04 13:00:41 +0100

    Add files via upload

--- toblerone.go
+++ toblerone.go
@@ -11,80 +11,108 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const keyFile = "key.txt"
 
+var encryptionKey string
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("  keygen              gen a new encryption key")
-		fmt.Println("  encrypt <input-file> <output-file> <encryption-key>   encrypt a file")
-		fmt.Println("  decrypt <input-file> <output-file> <sender-decryption-key>   decrypt a file")
-		fmt.Println("Your sender-decryption-key should be the text string sent along with the file")
-		fmt.Println("sender-decryption-key is essentially the senders encryption key")
+		fmt.Println("Usage: toblerone <command> [args]")
+		fmt.Println("  keygen              generate a new encryption key")
+		fmt.Println("  encrypt <inputFile.txt> <outputFile.tobl> <encryptionKey>   Encrypt a file")
+		fmt.Println("  decrypt <inputFile.tobl> <outputFile.txt> <senderDecryptionKey>   Decrypt a file")
 		return
 	}
 
 	command := os.Args[1]
 	switch command {
 	case "keygen":
-		err := moblerone()
+		err := Keygen()
 		if err != nil {
-			fmt.Println("error generating key:", err)
+			fmt.Println("Error generating key:", err)
 		}
 	case "encrypt":
 		if len(os.Args) < 5 {
-			fmt.Println("usage: toblerone encrypt <inputFile> <outputFile> <encryptionKey>")
+			fmt.Println("Usage: file-encrypt encrypt <inputFile> <outputFile> <encryptionKey>")
 			return
 		}
 		inputFile := os.Args[2]
 		outputFile := os.Args[3]
 		encryptionKey := os.Args[4]
 
-		err := foblerone(encryptionKey, inputFile, outputFile)
+		err := EncryptFile(encryptionKey, inputFile, outputFile)
 		if err != nil {
-			fmt.Println("error encrypting file:", err)
+			fmt.Println("Error encrypting file:", err)
 		}
 	case "decrypt":
 		if len(os.Args) < 5 {
-			fmt.Println("usage: toblerone decrypt <inputFile> <outputFile> <encryptionKey>")
+			fmt.Println("Usage: file-encrypt decrypt <inputFile> <outputFile> <encryptionKey>")
 			return
 		}
 		inputFile := os.Args[2]
 		outputFile := os.Args[3]
 		encryptionKey := os.Args[4]
 
-		err := zoblerone(encryptionKey, inputFile, outputFile)
+		err := DecryptFile(encryptionKey, inputFile, outputFile)
 		if err != nil {
-			fmt.Println("error decrypting file:", err)
+			fmt.Println("Error decrypting file:", err)
 		}
 	default:
-		fmt.Println("unknown command:", command)
+		fmt.Println("Unknown command:", command)
 	}
 }
 
-func moblerone() error {
-	key := make([]byte, 32)
-	_, err := rand.Read(key)
+// Keygen generates a new encryption key and saves it to a file.
+func Keygen() error {
+	fmt.Print("Enter encryption key: ")
+	key, err := readPassword()
 	if err != nil {
 		return err
 	}
 
-	return loblerone(key)
+	return SaveKeyToFile(key)
 }
 
-func loblerone(key []byte) error {
+// SaveKeyToFile saves the encryption key to a file after base64 encoding.
+func SaveKeyToFile(key []byte) error {
 	encodedKey := base64.StdEncoding.EncodeToString(key)
 	err := ioutil.WriteFile(keyFile, []byte(encodedKey), 0600)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("key saved to %s\n", keyFile)
+	fmt.Printf("Key saved to %s\n", keyFile)
 	return nil
 }
 
-func foblerone(encryptionKey, inputFile, outputFile string) error {
-	key := yucklerone(encryptionKey)
+// readPassword reads a password from the user without echoing it to the terminal.
+func readPassword() ([]byte, error) {
+	password := make([]byte, 0)
+
+	for {
+		char := make([]byte, 1)
+		_, err := os.Stdin.Read(char)
+		if err != nil {
+			return nil, err
+		}
+
+		// Break if Enter key is pressed
+		if char[0] == '\n' {
+			break
+		}
+
+		password = append(password, char[0])
+	}
+
+	return password, nil
+}
+
+// EncryptFile encrypts the contents of the input file and saves it to the output file with the .tobl extension.
+func EncryptFile(encryptionKey, inputFile, outputFile string) error {
+	key := deriveKey(encryptionKey)
 
 	plaintext, err := ioutil.ReadFile(inputFile)
 	if err != nil {
@@ -108,11 +136,15 @@ func foblerone(encryptionKey, inputFile, outputFile string) error {
 
 	encryptedData := append(iv, ciphertext...)
 
+	// Change the output file extension to .tobl
+	outputFile = changeFileExtension(outputFile, "tobl")
+
 	return ioutil.WriteFile(outputFile, encryptedData, 0644)
 }
 
-func zoblerone(encryptionKey, inputFile, outputFile string) error {
-	key := yucklerone(encryptionKey)
+// DecryptFile decrypts the contents of the input file and saves it to the output file.
+func DecryptFile(encryptionKey, inputFile, outputFile string) error {
+	key := deriveKey(encryptionKey)
 
 	encryptedData, err := ioutil.ReadFile(inputFile)
 	if err != nil {
@@ -139,7 +171,15 @@ func zoblerone(encryptionKey, inputFile, outputFile string) error {
 	return ioutil.WriteFile(outputFile, plaintext, 0644)
 }
 
-func yucklerone(keyString string) []byte {
+// deriveKey derives a 32-byte key from the provided encryption key using SHA-256.
+func deriveKey(keyString string) []byte {
 	hash := sha256.Sum256([]byte(keyString))
 	return hash[:]
+}
+
+// changeFileExtension changes the file extension of a given file path.
+func changeFileExtension(filePath, newExtension string) string {
+	ext := filepath.Ext(filePath)
+	newPath := strings.TrimSuffix(filePath, ext) + "." + newExtension
+	return newPath
 }
